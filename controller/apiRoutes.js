1a5d30507bc6043c161682c0bf2e8a234b054538
thossain <tanvirhossain2006@gmail.com>
2021-10-03 15:20:43 +1100

    Aggregate used in api instead of virtuals in models

--- controller/apiRoutes.js
+++ controller/apiRoutes.js
@@ -2,24 +2,33 @@ const router = require("express").Router();
 const db = require("../models");
 
 router.get("/workouts", (req, res) => {
-  db.Workout.find({})
-    .sort({ date: -1 })
+  db.Workout.aggregate([{
+    $addFields:{
+      totalDuration:{$sum: "$exercises.duration"}
+    }
+  }])
     .then((workout) => {
       res.status(200).json(workout);
     })
-    .catch((err) => {
-      res.status(400).json(err);
+    .catch((e) => {
+      res.status(400).json(e);
     });
 });
 
 router.get("/workouts/range", (req, res) => {
-  db.Workout.find({})
-    .sort({ date: -1 })
+  db.Workout.aggregate([{
+    $addFields:{
+      totalDuration:{$sum: "$exercises.duration"}
+    }
+  },
+  {$sort: {day: -1}}, 
+        {$limit: 7},
+])    
     .then((workout) => {
       res.status(200).json(workout);
     })
-    .catch((err) => {
-      res.status(400).json(err);
+    .catch((e) => {
+      res.status(400).json(e);
     });
 });
 
@@ -28,27 +37,28 @@ router.post("/workouts", (req, res) => {
     .then((workout) => {
       res.status(201).json(workout);
     })
-    .catch((err) => {
-      res.status(400).json(err);
+    .catch((e) => {
+      res.status(400).json(e);
     });
 });
 
 router.put("/workouts/:id", async (req, res) => {
   const id = req.params.id;
   const body = req.body;
-  db.Workout.updateOne(
+  db.Workout.findOneAndUpdate(
     { _id: id },
     {
       $push: {
         exercises: { ...body },
       },
-    }
+    },
+    { new: true }
   )
     .then((workout) => {
       res.status(200).json(workout);
     })
-    .catch((err) => {
-      res.status(400).json(err);
+    .catch((e) => {
+      res.status(400).json(e);
     });
 });
 
