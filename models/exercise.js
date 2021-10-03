c65d71c64b065f23c8c0f8e00cc8b460b4797c16
thossain <tanvirhossain2006@gmail.com>
2021-10-03 14:35:40 +1100

    code refactor pagesRoutes

--- models/exercise.js
+++ models/exercise.js
@@ -5,47 +5,39 @@ const Schema = mongoose.Schema;
 const exerciseSchema = new Schema ({
     
     type: {
-        type: String,
-        enum: ["resistance", "cardio"],
-        required: "Valid exercises are 'resistance' or 'cardio'",
+        type: String,        
+        required: "Enter the type of exercise",
+        trim: true,
       },
       name: {
         type: String,
         trim: true,
-        required: "Enter the type of exercise",
+        required: "Enter the name of exercise",
       },
       duration: {
         type: Number,
-        required: "Enter the duration in minutes",
+        
       },
       weight: {
         type: Number,
-        required: isRequired("weight"),
+        
       },
       reps: {
         type: Number,
-        required: isRequired("reps"),
+        
       },
       sets: {
         type: Number,
-        required: isRequired("sets"),
+        
       },
       distance: {
         type: Number,
-        required: isRequired("distance"),
+        
       }
 
 });
 
-function isRequired(field) {
-  return function () {
-    if (field == "distance") {
-      return this.type === "cardio";
-    } else {
-      return this.type === "resistance";
-    }
-  };
-}
+
 
 
 module.exports ={exerciseSchema};
